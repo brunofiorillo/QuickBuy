f3327fc9bc996c80d06ef62fbf4eabacd4853e3a
Bruno Fiorillo <bruno.l.fiorillo@gmail.com>
2020-08-05 14:48:27 -0300

    opa opa

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -6,6 +6,7 @@ import { Component } from "@angular/core";
   templateUrl: './login.component.html',
   styleUrls: ["./login.component.css"] 
 })
-export class LoginComponent { 
+export class LoginComponent {
+  public email = "bruno@teste.com";
 
 }
