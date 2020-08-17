a776ed5a2fe64b19ad9c786c84b84ab158f21633
Bruno Fiorillo <bruno.l.fiorillo@gmail.com>
2020-08-17 13:50:27 -0300

    17.08

---
+++ QuickBuy.Web/ClientApp/src/app/usuario/cadastro/cadastro.usuario.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from "@angular/core";
+import { Usuario } from "../../modelo/usuario";
+import { UsuarioServico } from "../../servicos/usuario/usuario.servico";
+
+@Component({
+  selector: "cadastro-usuario",
+  templateUrl: "./cadastro.usuario.component.html",
+  styleUrls: ["./cadastro.usuario.component.css"]
+})
+
+export class CadastroUsuarioComponent implements OnInit {
+  public usuario: Usuario;
+
+  constructor(private usuarioServico: UsuarioServico) {
+
+  }
+
+  ngOnInit(): void {
+    this.usuario = new Usuario;
+  }
+
+
+}
